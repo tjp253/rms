2de596d40c6255d398948d502bfa4dfa418317bf
Tom <tom@perdax.co.uk>
2021-11-17 16:58:32 +0000

    Added ability to specify axis when processing 2D arrays, etc.

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ Very simple module to calculate Root-Mean-Square values.
 import numpy as np
 
 
-def combine(x, n=2, period=1, mean=True, noise=False):
+def combine(x, n=2, period=1, mean=True, noise=False, axis=0):
     """ Combine several values, using a power of 'n' """
     if not hasattr(x, '__iter__'):  # Single value sent to function
         return x
@@ -20,20 +20,21 @@ def combine(x, n=2, period=1, mean=True, noise=False):
     period = np.asarray(period)
     if 1 < period.size < x.size:
         raise ValueError(
-                'If more than one period is entered, lengths must match.')
+                'If more than one period is entered, lengths must match.'
+            )
     x = x * period  # Account for time. Used when sample intervals differ.
 
     nans = np.isnan(x)
     if nans.all():
         return np.nan
 
-    x = np.nansum(x, axis=0)  # SUM
+    x = np.nansum(x, axis=axis)  # SUM
 
     if mean:  # MEAN
         if period.size == 1:
-            divider = np.nansum(~nans, axis=0) * period
+            divider = np.nansum(~nans, axis=axis) * period
         else:
-            divider = np.nansum(period, axis=0)
+            divider = np.nansum(period, axis=axis)
 
         x /= divider
 
@@ -44,6 +45,6 @@ def combine(x, n=2, period=1, mean=True, noise=False):
     return x
 
 
-def rms(x, period=1, noise=False):
+def rms(x, period=1, noise=False, axis=0):
     """ Calculate the Root-Mean-Square of 'x' """
-    return combine(x, n=2, period=period, noise=noise)
+    return combine(x, n=2, period=period, noise=noise, axis=axis)
